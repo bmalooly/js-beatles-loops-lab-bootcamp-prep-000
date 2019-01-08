99296feec22fa6ed05b94f7ea769119860c3f2dd
Brittany Malooly <bmalooly@gmail.com>
2019-01-08 01:02:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 function theBeatlesPlay(musicians, instruments) {
   var people = [] 
   for (let i = 0; i < 2; i++) { people.push(`musicians.slice(-1) plays instruments.slice(-1)`) }
+  return people
 }
