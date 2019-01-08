8355d9ca613a60ee52f87efd1d8f23c89551cdba
Brittany Malooly <bmalooly@gmail.com>
2019-01-08 01:34:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ function theBeatlesPlay(musicians, instruments) {
 function johnLennonFacts() {
   var facts = [`He was the last Beatle to learn to drive`, `He was never a vegetarian`, `He was a choir boy and boy scout`, `He hated the sound of his own voice`]
   var array = []
-  var i = 0
-  while (i < facts.length) return facts[i] + "!!!"
+  while (array.length < facts.length) return facts[i] + "!!!"
 }
   
