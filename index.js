7286b2669b582dbb591e944600a022a1c0382c97
Brittany Malooly <bmalooly@gmail.com>
2019-01-08 01:51:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ function johnLennonFacts(facts) {
   }
   function iLoveTheBeatles(num) {
     var beatles = []
-    do { beatles.push(`I love the Beatles!`)
-    while
+    do { beatles.push(`I love the Beatles!`);
+  }  while (num < 15);
+  return beatles ++ 'I love the Beatles!';
   }
