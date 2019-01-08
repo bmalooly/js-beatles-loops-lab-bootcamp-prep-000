7197624464843937a9bd68198faa7206f10876d7
Brittany Malooly <bmalooly@gmail.com>
2019-01-08 01:08:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments) {
   var people = [];
-  for (let i = 0; i < 2; i++) { 
+  for (let i = 0; i < 3; i++) { 
     people.push(musicians[i] + ` plays ` + instruments[i]);
   }
   return people;
