83da01d391c9558c9251355859e9a523ea9d23f3
Brittany Malooly <bmalooly@gmail.com>
2019-01-08 01:21:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,5 +7,6 @@ function theBeatlesPlay(musicians, instruments) {
 }
 function johnLennonFacts() {
   var facts = [`He was the last Beatle to learn to drive`, `He was never a vegetarian`, `He was a choir boy and boy scout`, `He hated the sound of his own voice`]
+  var i = 0
   while (i < 5) return facts[i] + "!!!"}
   
