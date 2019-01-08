90fc2815c423f045bb116900bdc6e2e4422c3dfd
Brittany Malooly <bmalooly@gmail.com>
2019-01-08 01:44:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function johnLennonFacts() {
   var array = [];
   var i = 0;
   while (i < facts.length) { 
-  array.push(`${facts[i]}' + "!!!")
+  array.push(facts[i] + "!!!")
   }
   return array;
   }
